e4c912c02158d2345d9d70a7fdcf2d908ff56e2a
tylix <bytydoo@gmail.com>
2021-04-08 23:41:26 +0200

    Update AnswerCommand.java

--- src/main/java/net/verany/messaging/command/AnswerCommand.java
+++ src/main/java/net/verany/messaging/command/AnswerCommand.java
@@ -1,5 +1,7 @@
 package net.verany.messaging.command;
 
+import net.verany.messaging.VeranyMessenger;
+import net.verany.messaging.websocket.WebSocketClient;
 import net.verany.messaging.websocket.WebSocketRequest;
 
 public class AnswerCommand extends Command {
@@ -10,6 +12,9 @@ public class AnswerCommand extends Command {
 
     @Override
     public void onExecute(WebSocketRequest request) {
-
+        String key = request.getRequest().getString("id");
+        WebSocketClient client = VeranyMessenger.INSTANCE.getSocketManager().getClient(key);
+        if (client == null) return;
+        client.send(request.getRequest().toString());
     }
 }
